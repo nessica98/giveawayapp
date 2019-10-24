5e7f0a4478c29da42ab3aff1905f56193cd7feef
Jisung Hwang <poksin.w@gmail.com>
2019-10-25 00:19:49 +0700

    update login version 1

--- backend/user.model.js
+++ backend/user.model.js
@@ -1,19 +1,39 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    minlength: 3
-  },
-  password: String,
-  email: String,
-  TwitterUser: String
+  username: String,
+  hash: String,
+  salt: String
 });
+
+userSchema.methods.setPassword = function(password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt,10000,512,'sha512').toString('hex');
+}
+
+userSchema.methods.generateJWT = function(password) {
+  const today = new Date();
+  const expirationDate = new Date(today);
+  expirationDate.setDate(today.getDate() + 60)
+
+  return jwt.sign({
+    email: this.email,
+    id: this._id,
+    exp: parseInt(expirationDate.getTime() / 1000, 10)
+  },'secret');
+}
+
+userSchema.methods.toAuthJSON = function() {
+  return {
+    _id: this._id,
+    email: this.email,
+    token: this.generateJWT()
+  }
+}
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
