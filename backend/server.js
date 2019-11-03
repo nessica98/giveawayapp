670bf529597ab35c6a958bc0a6c9caccf48ba3d6
Jisung Hwang <poksin.w@gmail.com>
2019-11-03 14:41:23 +0700

    Update backend

--- backend/server.js
+++ backend/server.js
@@ -15,13 +15,10 @@ const giveaway = require('./giveaway')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-//require('./config/passport')
-//app.use(require('./routes'))
+
 
 app.use(cors());
 app.use(express.json());
-
-app.use(session({secret: 'passport-test',cookie:{maxAge: 60000}, resave:false, saveUninitialized:false}))
 
 
 app.use('/user',Users);
@@ -29,7 +26,7 @@ app.use('/giveaway', giveaway);
 
 const uri = process.env.ATLAS_URI;
 //console.log(uri)
-mongoose.connect(uri, { useUnifiedTopology: true ,useNewUrlParser: true, useCreateIndex: true }
+mongoose.connect(uri, { useFindAndModify: false,useUnifiedTopology: true ,useNewUrlParser: true, useCreateIndex: true }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
