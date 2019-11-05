f08f7c1573e2519f59731fcb57543f6c009b7f67
Jisung Hwang <poksin.w@gmail.com>
2019-11-05 19:35:29 +0700

    server only init to deploy

--- src/component/reducer/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux'
-import loggedReducer from './logged';
-import room from './room';
-
-export default combineReducers({loggedReducer,room})
--- src/test2.js
+++
@@ -1,8 +0,0 @@
-a = [1,2,3,4,5]
-
-// .map .find .findIndex
-
-console.log(a.findIndex((b)=>{
-    console.log("data is ", b)
-    return b == 3
-}))
