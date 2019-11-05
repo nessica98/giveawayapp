8ea547faaee1f458f85d9e22742affecd47d8b19
Jisung Hwang <poksin.w@gmail.com>
2019-11-05 19:26:49 +0700

    only server

--- README.md
+++
@@ -1,3 +0,0 @@
-# 
-backend command run : `nodemon server`
-server will run on http://localhost:5000
--- src/component/reducer/room.js
+++
@@ -1,6 +0,0 @@
-const room = (room = '',action) => {
-    switch(action.type){
-        case 'changeroom' :
-            return action.name
-    }
-}
